c28deddacfa85b39d2d64e3e0d558441a496b9a6
Byron Dowling <byrondowling7@gmail.com>
2021-11-24 19:00:25 -0600

    Create 12468_Zapping_BGD.cpp

---
+++ Assignments/12468 Zapping/12468_Zapping_BGD.cpp
@@ -0,0 +1,45 @@
+/*
+    Name:       Byron Dowling
+    Course:     4883 Programming Techniques
+    Date:       11/27/2021
+
+    Assignment: 12468 Zapping
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int x, y = 0;
+  int r1, r2 = 0;
+  bool complete = false;
+
+  while (!complete)
+  {
+    cin >> x >> y;
+
+    if (x == -1 && y == -1)
+    {
+      complete = true;
+      break;
+    }
+
+    r1 = abs(x - y);
+
+    if (y > x)
+    {
+      r2 = ((100 - y) + x);
+    }
+
+    else
+    {
+      r2 = ((100 - x) + y);
+    }
+
+    cout << min(r1, r2) << endl;
+  }
+  
+  return 0;
+} 
