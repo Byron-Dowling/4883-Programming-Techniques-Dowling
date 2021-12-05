cacdb14f0a5758456ac68fa349f993937b950fdc
Byron Dowling <byrondowling7@gmail.com>
2021-12-05 14:11:37 -0600

    Create sorted_generator.py

---
+++ Assignments/Sorting Experiment/sorted_generator.py
@@ -0,0 +1,27 @@
+import random
+import math
+import sys
+
+def main(n,m=2**30):
+
+    with open("reversesortednums.dat","w") as f:
+        f.write(str(n)+" ")
+        for i in range(n):
+            r = n - i
+            # r = i + 1
+            f.write(str(r)+" ")
+
+if __name__=='__main__':
+    if len(sys.argv) == 1:
+        n = 10000
+        m = 2**30
+    elif len(sys.argv) == 2:
+        n = int(sys.argv[1])
+        m = 2**30
+    elif len(sys.argv) == 3:
+        n = int(sys.argv[1])
+        m = int(sys.argv[2])
+
+    main(n,m)
+
+    print(2**30)
